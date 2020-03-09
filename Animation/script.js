40dce2c736696ef15ffb7c3be66bc27df1e9ec90
Val02 <55113112+Val02@users.noreply.github.com>
2020-03-09 14:38:17 -0400

    Add files via upload

---
+++ Animation/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  
+  setTimeout(function() {
+    $(".bg").addClass("hide");
+    $(".yoyooneback").addClass("hide");
+    $(".yoyotwoback").addClass("hide");
+    $(".yoyothreeback").addClass("hide");
+    $(".yoyoonefront").addClass("hide");
+    $(".yoyotwofront").addClass("hide");
+    $(".yoyothreefront").addClass("hide");
+    $(".alarmbg").removeClass("hide");
+    $(".snoozebutton").removeClass("hide");
+  }, 6000);
+});
