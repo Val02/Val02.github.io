ba29a290c9886f3f79af97d97981b6b636ec77f9
Val02 <55113112+Val02@users.noreply.github.com>
2020-03-28 19:10:01 -0400

    Add files via upload

--- demo/script.js
+++ demo/script.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
   var mugen = new MUGEN();
-  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 150, timeBetweenNPCs: 2900, npcCollisionRadius: 150, actionDuration:700, jumpHeight: 350});
+  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 150, timeBetweenNPCs: 2900, npcCollisionRadius: 50, actionDuration:600, jumpHeight: 350});
 });
